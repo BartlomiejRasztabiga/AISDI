69abb92eb80cc0bbfc3e55d80d8236fa25d2e1c2
BartlomiejRasztabiga <contact@rasztabiga.me>
2021-03-22 09:29:46 +0100

    Simplify merge_sort

--- zad2/algorithms.py
+++ zad2/algorithms.py
@@ -32,5 +32,20 @@ def merge_sort(arr):
     return merge(left, right)
 
 
-def quick_sort():
-    pass
+def quick_sort(arr):
+    less = []
+    equal = []
+    greater = []
+
+    if len(arr) > 1:
+        pivot = arr[0]
+        for x in arr:
+            if x < pivot:
+                less.append(x)
+            elif x == pivot:
+                equal.append(x)
+            elif x > pivot:
+                greater.append(x)
+        return quick_sort(less) + equal + quick_sort(greater)
+    else:
+        return arr
