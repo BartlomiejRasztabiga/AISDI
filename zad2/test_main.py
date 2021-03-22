06c4d9438baecaaf5932230d14fa11dc06968978
BartlomiejRasztabiga <contact@rasztabiga.me>
2021-03-22 09:10:51 +0100

    Add basic tests for merge_sort and quick_sort

--- zad2/test_main.py
+++ zad2/test_main.py
@@ -1,2 +1,17 @@
-def test():
-    assert True is True
+from algorithms import merge_sort, quick_sort
+
+
+def test_merge_sort_numbers():
+    assert merge_sort([3, 5, 1]) == [1, 3, 5]
+
+
+def test_merge_sort_strings():
+    assert merge_sort(["gfd", "aba", "dsa"]) == ["aba", "dsa", "gfd"]
+
+
+def test_quick_sort_numbers():
+    assert quick_sort([3, 5, 1]) == [1, 3, 5]
+
+
+def test_quick_sort_strings():
+    assert quick_sort(["gfd", "aba", "dsa"]) == ["aba", "dsa", "gfd"]
