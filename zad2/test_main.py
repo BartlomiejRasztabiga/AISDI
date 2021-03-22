539969e1347689299aba099b96f1e14756c1ed52
BartlomiejRasztabiga <contact@rasztabiga.me>
2021-03-22 09:27:16 +0100

    Add more data to tests

--- zad2/test_main.py
+++ zad2/test_main.py
@@ -2,16 +2,20 @@ from algorithms import merge_sort, quick_sort
 
 
 def test_merge_sort_numbers():
-    assert merge_sort([3, 5, 1]) == [1, 3, 5]
+    assert merge_sort([3, 44, 38, 5, 47, 15, 36, 26, 27, 2, 46, 4, 19, 50, 48]
+                      ) == [2, 3, 4, 5, 15, 19, 26, 27, 36, 38, 44, 46, 47, 48, 50]
 
 
 def test_merge_sort_strings():
-    assert merge_sort(["gfd", "aba", "dsa"]) == ["aba", "dsa", "gfd"]
+    assert merge_sort(["C59OK", "5QV09", "TCNId", "ne58v", "ges28", "EzMbM", "qdPku", "ltinw", "Nt1Rj", "nSb8R"]
+                      ) == ["5QV09", "C59OK", "EzMbM", "Nt1Rj", "TCNId", "ges28", "ltinw", "nSb8R", "ne58v", "qdPku"]
 
 
 def test_quick_sort_numbers():
-    assert quick_sort([3, 5, 1]) == [1, 3, 5]
+    assert quick_sort([3, 44, 38, 5, 47, 15, 36, 26, 27, 2, 46, 4, 19, 50, 48]
+                      ) == [2, 3, 4, 5, 15, 19, 26, 27, 36, 38, 44, 46, 47, 48, 50]
 
 
 def test_quick_sort_strings():
-    assert quick_sort(["gfd", "aba", "dsa"]) == ["aba", "dsa", "gfd"]
+    assert quick_sort(["C59OK", "5QV09", "TCNId", "ne58v", "ges28", "EzMbM", "qdPku", "ltinw", "Nt1Rj", "nSb8R"]
+                      ) == ["5QV09", "C59OK", "EzMbM", "Nt1Rj", "TCNId", "ges28", "ltinw", "nSb8R", "ne58v", "qdPku"]
