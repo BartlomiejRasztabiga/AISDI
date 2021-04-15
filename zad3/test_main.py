bc75fe056ad5687353d590b04f9af60f8652d464
BartlomiejRasztabiga <contact@rasztabiga.me>
2021-04-16 01:35:50 +0200

    Add few more tests

--- zad3/test_main.py
+++ zad3/test_main.py
@@ -47,6 +47,31 @@ def test_delete_left():
     assert tree.find_node(-5) is None
 
 
+def test_delete_no_children():
+    tree = BST([10])
+    tree.insert_node(0)
+    tree.insert_node(20)
+    tree.insert_node(-5)
+    tree.insert_node(15)
+    tree.insert_node(1)
+    tree.insert_node(-10)
+
+    tree.remove_node(-10)
+
+    assert len(tree) == 6
+    assert tree.root.left.left.item == -5
+    assert tree.find_node(-10) is None
+
+
+def test_delete_single_node():
+    tree = BST([10])
+
+    tree.remove_node(10)
+
+    assert len(tree) == 0
+    assert tree.find_node(-10) is None
+
+
 def test_delete_left_with_duplicates():
     tree = BST([10])
     tree.insert_node(0)
@@ -92,9 +117,25 @@ def test_delete_non_existing():
         tree.remove_node(100)
 
 
-def test_print():
+def test_print_tree():
     tree = BST([10])
     tree.insert_node(0)
     tree.insert_node(20)
 
     assert tree.__str__() == "\n    20\n  10\n    0\n\n"
+
+
+def test_print_node():
+    tree = BST([10])
+    tree.insert_node(0)
+    tree.insert_node(20)
+
+    assert tree.root.__str__() == "  20\n10\n  0\n"
+
+
+def test_contains_method():
+    tree = BST([10])
+    tree.insert_node(0)
+    tree.insert_node(20)
+
+    assert 0 in tree
