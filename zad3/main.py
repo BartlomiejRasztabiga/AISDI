4c75bacba5ceecc037e79e612306ad8cd47346b2
Piotrek Brzeziński <thewildtree@outlook.com>
2021-04-18 00:14:43 +0200

    WIP: element deletion benchmarks

--- zad3/main.py
+++ zad3/main.py
@@ -8,9 +8,10 @@ num_of_repetitions = 3  # num of repetitions to get avg time
 
 
 def main():
-    tree_creation_results = compare_tree_creation()
+    #tree_creation_results = compare_tree_creation()
 
-    print(tree_creation_results)
+    #print(tree_creation_results)
+    print(compare_node_removal())
 
 
 def compare_tree_creation():
@@ -25,6 +26,32 @@ def compare_tree_creation():
         results[n] = (bst_results, avl_results)
 
     return results
+
+
+def compare_node_removal():
+    results = {}
+    count = [(n + 1) * 100 for n in range(50)]
+    for n in count:
+        # constant amount of input elements throughout all tests
+        content = generate_input_data(10000)
+
+        # bst_results = run_node_removal(BST, content, n)
+        avl_results = run_node_removal(AVL, content, n)
+        results[n] = avl_results
+
+    return results
+
+
+def run_node_removal(tree_class, content, count):
+    time = 0
+
+    for _ in range(num_of_repetitions):
+        tree = tree_class(content)
+        nodes_to_remove = content[:count]
+        timer = timeit.Timer(lambda _tree=tree: _tree.remove_nodes(nodes_to_remove))
+        time += timer.timeit(1)
+
+    return time / num_of_repetitions
 
 
 def run_bst_tree_creation(content):
