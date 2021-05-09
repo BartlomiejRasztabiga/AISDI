f64a5310bd9efc19bd74a333d6b345ff1a17e554
BartlomiejRasztabiga <contact@rasztabiga.me>
2021-05-09 17:14:30 +0200

    Add more tests

--- zad4/test_main.py
+++ zad4/test_main.py
@@ -1,4 +1,4 @@
-from heap import BinaryHeap
+from heap import BinaryHeap, TernaryHeap, QuarternaryHeap
 
 
 class TestBinaryHeap:
@@ -21,3 +21,33 @@ class TestBinaryHeap:
 
         assert len(heap) == 5
         assert heap[0] == -3
+
+
+class TestTernaryHeap:
+    def test_insert_simple(self):
+        heap = TernaryHeap()
+        heap.push(5)
+        heap.push(1)
+        heap.push(2)
+        heap.push(-1)
+        heap.push(10)
+
+        assert len(heap) == 5
+        assert heap[0] == -1
+
+
+class TestQuaternaryHeap:
+    def test_insert_simple(self):
+        heap = QuarternaryHeap()
+        heap.push(5)
+        heap.push(1)
+        heap.push(2)
+        heap.push(-1)
+        heap.push(10)
+        heap.push(7)
+        heap.push(-7)
+        heap.push(9)
+        heap.push(-11)
+
+        assert len(heap) == 9
+        assert heap[0] == -11
