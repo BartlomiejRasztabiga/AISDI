3a975c4d8d0fd3af7663d47f69856ae0f41c7ad8
BartlomiejRasztabiga <contact@rasztabiga.me>
2021-05-20 13:12:58 +0200

    Add stubs for algorithms

---
+++ zad5/algorithms.py
@@ -0,0 +1,34 @@
+def find_naive(string, text):
+    """
+    Naive
+    Parameters:
+        string (str): szukany napis
+        text (str): przeszukiwany tekst
+    Returns:
+        (list): lista pozycji w 'text' (w kolejności rosnącej), od których zaczyna się 'string'
+    """
+    pass
+
+
+def find_kmp(string, text):
+    """
+    Knuth-Morris-Pratt
+    Parameters:
+        string (str): szukany napis
+        text (str): przeszukiwany tekst
+    Returns:
+        (list): lista pozycji w 'text' (w kolejności rosnącej), od których zaczyna się 'string'
+    """
+    pass
+
+
+def find_kr(string, text):
+    """
+    Karp-Rabin
+    Parameters:
+        string (str): szukany napis
+        text (str): przeszukiwany tekst
+    Returns:
+        (list): lista pozycji w 'text' (w kolejności rosnącej), od których zaczyna się 'string'
+    """
+    pass
