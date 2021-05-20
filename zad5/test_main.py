54f4048c09985ec97ab9e01a0a9b5c66600f5d97
BartlomiejRasztabiga <contact@rasztabiga.me>
2021-05-20 13:14:18 +0200

    Add stubs for tests

---
+++ zad5/test_main.py
@@ -0,0 +1,13 @@
+class TestNaive:
+    def test_simple(self):
+        pass
+
+
+class TestKMP:
+    def test_simple(self):
+        pass
+
+
+class TestKR:
+    def test_simple(self):
+        pass
